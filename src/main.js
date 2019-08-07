3a8d3bf0e73f7ecfa5feeb7b4a748f56b91fc7e3
htl-leonding <t.stuetz@htl-leonding.ac.at>
2019-08-07 19:15:55 +0200

    Server list is now clickable; in the server details the status is shown

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+export const serverBus = new Vue();
 
 new Vue({
   render: h => h(App),
